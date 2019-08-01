e4fe63e299f18b4f257b5a9b791b86e0c06df4bf
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-01 18:24:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,10 +48,8 @@ def update_counting_hash(hash, key)
    
    if update_counting_hash[key]
      update_counting_hash[key] += 1
-     p update_counting_hash
    else 
-     update_counting_hash[key] = 1
-     p update_counting_hash
+     update_counting_hash[key] = 1 
    end 
 
 end
